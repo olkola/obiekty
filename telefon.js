61c60a3c9599cbd87c3ac3d039d141e9cd1a7e1d
olkola <juego@o2.pl>
2016-10-11 14:29:41 +0200

    Poprawiona kolejność parametrów

--- telefon.js
+++ telefon.js
@@ -6,9 +6,9 @@ function Telefon(marka, cena, kolor) {
 Telefon.prototype.printInfo = function() {
 		console.log('Marka telefonu to ' + this.marka + ', kolor to ' + this.kolor + ', a cena to ' + this.cena + '.');
 };
-var samsungGalaxyS6 = new Telefon('Samsung', 'czarny', 1861);
-var iPhone6S = new Telefon('Apple', 'srebrny', 2250);
-var oneplusOne= new Telefon('OnePlus One', 'biały', 1174);
+var samsungGalaxyS6 = new Telefon('Samsung', 1861, 'czarny');
+var iPhone6S = new Telefon('Apple', 2250, 'srebrny');
+var oneplusOne= new Telefon('OnePlus One', 1174, 'biały');
 samsungGalaxyS6.printInfo();
 iPhone6S.printInfo();
 oneplusOne.printInfo();
